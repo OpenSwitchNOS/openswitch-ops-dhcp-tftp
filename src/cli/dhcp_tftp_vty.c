227babbe9252cc41d7694130f0ddded7b9934c57
Srinivasan Srivatsan <srinivasan.srivatsan@hpe.com>
2016-05-24 12:00:35 -0700

    chg: dev: DHCP 2 mins minimum lease time config
    Min lease time is 2 mins as per
    http://linux.die.net/man/8/dnsmasq
    
    Change-Id: Id4066d44c3218d45297b50d5e96a1a22271331bd
    Signed-off-by: Srinivasan Srivatsan <srinivasan.srivatsan@hpe.com>

--- src/cli/dhcp_tftp_vty.c
+++ src/cli/dhcp_tftp_vty.c
@@ -2884,7 +2884,7 @@ DEFUN (cli_dhcp_server_static_host_delete,
        "no static (A.B.C.D|X:X::X:X) "
        "{match-mac-addresses MAC-ADDRs | set tags TAGs | "
        "match-client-hostname WORD | match-client-id WORD | "
-       "lease-duration LEASE-TIME}",
+       "lease-duration 0/<2-65535>}",
        "Remove DHCP server configuration\n"
        "Static leases configuration\n"
        "Enter host static IPv4 address\n"
@@ -2903,7 +2903,7 @@ DEFUN (cli_dhcp_server_static_host_delete,
 {
     int ret_code = CMD_SUCCESS;
     dhcp_srv_static_host_params_t static_host_params;
-
+    uint32_t lease_duration;
     /* validating ip address */
     if (!is_valid_ip_address((char *)argv[0])) {
         vty_out(vty, "%s is invalid%s",argv[0],VTY_NEWLINE);
@@ -2975,7 +2975,13 @@ DEFUN (cli_dhcp_server_static_host_delete,
     if (argv[5] == NULL) {
         static_host_params.lease_duration = 60;
     } else {
-        static_host_params.lease_duration = atoi(argv[5]);
+        lease_duration = atoi((char *) argv[5]);
+        if (lease_duration == 1 || lease_duration > 65535) {
+            vty_out(vty, \
+            "Lease duration should be 0 for infinite or between 2-65535%s", VTY_NEWLINE);
+            return CMD_SUCCESS; /* check */
+        }
+        static_host_params.lease_duration = lease_duration;
     }
 
     ret_code = dhcp_server_delete_static_host(&static_host_params);
@@ -2988,7 +2994,7 @@ DEFUN (cli_dhcp_server_static_host_add,
        "static (A.B.C.D|X:X::X:X) "
        "{match-mac-addresses MAC-ADDRs | set tags TAGs | "
        "match-client-hostname WORD | "
-       "match-client-id WORD | lease-duration LEASE-TIME}",
+       "match-client-id WORD | lease-duration 0/<2-65535>}",
        "Static leases configuration\n"
        "Enter host static IPv4 address\n"
        "Enter host static IPv6 address\n"
@@ -3002,11 +3008,12 @@ DEFUN (cli_dhcp_server_static_host_add,
        "Match DHCP client-id\n"
        "Enter DHCP client-id\n"
        "DHCP lease duration\n"
-       "Enter DHCP lease duration time (Default: 60 minutes)\n")
+       "Enter DHCP lease duration in minutes, 0 "\
+       "for \"infinite\",(Default: 60 minutes)\n")
 {
     int ret_code = CMD_SUCCESS;
     dhcp_srv_static_host_params_t static_host_params;
-
+    uint32_t lease_duration;
     /* validating ip address */
     if (!is_valid_ip_address((char *)argv[0])) {
         vty_out(vty, "%s is invalid%s",argv[0],VTY_NEWLINE);
@@ -3075,7 +3082,13 @@ DEFUN (cli_dhcp_server_static_host_add,
         static_host_params.lease_duration = 60;
 
     } else {
-        static_host_params.lease_duration = atoi(argv[5]);
+        lease_duration = atoi((char *) argv[5]);
+        if (lease_duration == 1 || lease_duration > 65535) {
+            vty_out(vty, \
+            "Lease duration should be 0 for infinite or between 2-65535%s", VTY_NEWLINE);
+            return CMD_SUCCESS; /* check */
+        }
+        static_host_params.lease_duration = lease_duration;
     }
 
     ret_code = dhcp_server_add_static_host(&static_host_params);
@@ -3089,7 +3102,7 @@ DEFUN (cli_dhcp_server_range_delete,
        " end-ip-address (A.B.C.D|X:X::X:X) {static "
        "| set tag TAG-NAME | match tags TAGS "
        "| netmask A.B.C.D | broadcast A.B.C.D | prefix-len <64-128> | "
-       "lease-duration <0-65535>}",
+       "lease-duration 0/<2-65535>}",
        "Remove DHCP server configuration\n"
        "DHCP server IP address range configuration\n"
        "Enter DHCP server IP address range name\n"
@@ -3124,6 +3137,7 @@ DEFUN (cli_dhcp_server_range_delete,
     bool end_ip_ipv6 = false;
     dhcp_srv_range_params_t range_params;
     int ret_code;
+    uint32_t lease_duration;
     if (argv[0] == NULL) {
         vty_out(vty, "Error: Name is not configured%s",VTY_NEWLINE);
         return CMD_ERR_INCOMPLETE;
@@ -3290,7 +3304,13 @@ DEFUN (cli_dhcp_server_range_delete,
     if (argv[9] == NULL) {
         range_params.lease_duration = 60;
     } else {
-        range_params.lease_duration = atoi((char *)argv[9]);
+        lease_duration = atoi((char *) argv[9]);
+        if (lease_duration == 1 || lease_duration > 65535) {
+            vty_out(vty, \
+            "Lease duration should be 0 for infinite or between 2-65535%s", VTY_NEWLINE);
+            return CMD_SUCCESS; /* check */
+        }
+        range_params.lease_duration = lease_duration;
     }
 
 
@@ -3305,7 +3325,7 @@ DEFUN (cli_dhcp_server_range_add,
        " end-ip-address (A.B.C.D|X:X::X:X) {static "
        "| set tag TAG-NAME | match tags TAGS "
        "| netmask A.B.C.D | broadcast A.B.C.D | prefix-len <64-128> | "
-       "lease-duration <0-65535>}",
+       "lease-duration 0/<2-65535>}",
        "DHCP server IP address range configuration\n"
        "Enter DHCP server IP address range name\n"
        "Start IP address\n"
@@ -3337,6 +3357,8 @@ DEFUN (cli_dhcp_server_range_add,
     bool end_ip_ipv6 = false;
     dhcp_srv_range_params_t range_params;
     int ret_code;
+    uint32_t lease_duration;
+
     if (argv[0] == NULL) {
         vty_out(vty, "Error: Name is not configured%s ",
                       VTY_NEWLINE);
@@ -3503,7 +3525,13 @@ IPv4 %s", VTY_NEWLINE);
     if (argv[9] == NULL) {
         range_params.lease_duration = 60;
     } else {
-        range_params.lease_duration = atoi((char *)argv[9]);
+        lease_duration = atoi((char *) argv[9]);
+        if (lease_duration == 1 || lease_duration > 65535) {
+            vty_out(vty, \
+            "Lease duration should be 0 for infinite or between 2-65535%s", VTY_NEWLINE);
+            return CMD_SUCCESS; /* check */
+        }
+        range_params.lease_duration = lease_duration;
     }
 
     ret_code = dhcp_server_add_range(&range_params);
